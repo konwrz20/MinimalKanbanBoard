4ce61f9f4b0513c2f7aa276eb09fbbfcc3509748
konwrz20 <konwrz20@gmail.com>
2020-01-17 22:38:50 +0100

    configured vue project

---
+++ web/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+    '@vue/typescript',
+    '@vue/prettier',
+    'prettier/@typescript-eslint'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
